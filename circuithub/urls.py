c8c7f1c42b3ad88808cc28905e73dc65db302154
GorioB <gmcbuencamino@gmail.com>
2014-02-28 21:15:00 +0800

    fixed main urls.py, fingers crossed for heroku working properly

--- circuithub/urls.py
+++ circuithub/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings #for serving static files
 from django.contrib import admin
+from django.conf.urls.static import static
 admin.autodiscover()
 
-urlpatterns = patterns('',
+
+urlpatterns= patterns('',
     # Examples:
     # url(r'^$', 'logintest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^upload/',include('upload.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^u/',include('circuits.urls')),
     url(r'^login/','loginapp.views.login_view'),
     url(r'^logout/','loginapp.views.logout_view'),
     url(r'^$','loginapp.views.index_view'),
     url(r'^register/','loginapp.views.register'),
-    url(r'^register_submit/','loginapp.views.register_submit')
+    url(r'^register_submit/','loginapp.views.register_submit'),
 )
